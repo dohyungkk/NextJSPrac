2f7671b92e3ba583e8daa86f9bde793b63d9d2e4
Kevin Kim <kdkim7676@gmail.com>
2023-10-29 00:08:11 -0700

    redirect added to keep changes only viewable on server side

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,7 +1,17 @@
 import Link from "next/link";
+import prisma from "../db"
+import { redirect } from "next/navigation";
 
 async function createTodo(data: FormData) {
     "use server"
+
+    const title = data.get("title")?.valueOf()
+    if (typeof title !== "string" || title.length === 0) {
+        throw new Error("Invalid Title")
+    }
+
+    await prisma.todo.create({ data: { title, complete: false }})
+    redirect("/")
 }
 
 export default function Page() {
@@ -10,7 +20,7 @@ export default function Page() {
           <header className="flex justify-between items-center mb-4">
             <h1 className="text-2xl">New</h1>
           </header>
-          <form>
+          <form action={createTodo} className="flex gap-2 flex-col">
             <input 
                 type="text" 
                 name="title" 
